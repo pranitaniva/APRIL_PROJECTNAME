24a1487a0b9e283f33f22ab4380bea3edb4c0306
pranitaniva <130834667+pranitaniva@users.noreply.github.com>
2023-04-15 18:59:59 +0530

    Update A_1_1_Demo.java

--- GITHUB_Practice/src/A_1_1_pracice/A_1_1_Demo.java
+++ GITHUB_Practice/src/A_1_1_pracice/A_1_1_Demo.java
@@ -6,5 +6,6 @@ public static void main(String[] args)
 {
 	System.out.println("hi this is my first github push");
 	System.out.println("hi");
+	System.out.println("change done by another person");
 }
 }
